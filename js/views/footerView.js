ef20598a3f7c62f136bf245e0cac621e024a0a04
Adam Gadziak <adamgadziak@gmail.com>
2015-04-22 04:40:17 +0200

    footer active upgrade

--- js/views/footerView.js
+++ js/views/footerView.js
@@ -4,6 +4,9 @@ UAM.footerView = function (content) {
 	this.setCount = function(count) {
     this.content.querySelector('#countAll').textContent = "All items: " + count;
 	};
+	this.setActiveCount = function(count) {
+	this.content.querySelector('#countActive').textContent = "Active items: " + count;
+	};
 }
 
 UAM.utils.inherits(UAM.basicView, UAM.footerView);
